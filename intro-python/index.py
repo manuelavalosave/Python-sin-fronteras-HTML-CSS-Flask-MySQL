33347dfd72a549be9c8c322a1076253f59b42426
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2021-03-21 16:55:01 -0600

    Create index.py
    manejo de variables

---
+++ intro-python/index.py
@@ -0,0 +1,34 @@
+#aca va un comentario
+if 3 > 5 :
+    print("Esto no se impreme en pantalla")
+
+#if 5 > 3 : #Esto si se cumple
+    #print("5 es mayor que 3")
+
+#creando sintaxis de variables
+x = 5
+y = 'soy una variable'
+
+#imprimo las dos variables
+print(x, y)
+
+#asigno una nueva variable
+correo = 'correo@demo.com'
+
+print(correo)
+
+#Asignar datos en una misma linea
+a, b, c = 'LaLA','LeLe', 'LiLi'
+
+print(a, b, c)
+
+#Mandamos el mismo valor de una variable
+valor1 = valor2 = valor3 = "Chuchito feliz"
+
+print(valor1, valor2, valor3)
+
+#contatenar dos varaibles
+inicio = 'Hola '
+final = 'Mundo'
+
+print(inicio + final)
