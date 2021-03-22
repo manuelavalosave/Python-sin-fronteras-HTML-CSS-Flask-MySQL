eb98457b60cedef8943c0eee41fd971f03b68896
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2021-03-22 14:41:57 -0600

    Update index.py

--- intro-python/index.py
+++ intro-python/index.py
@@ -15,7 +15,7 @@ print(x, y)
 #asigno una nueva variable
 correo = 'correo@demo.com'
 
-print(correo)
+# print(correo)
 
 #Asignar datos en una misma linea
 a, b, c = 'LaLA','LeLe', 'LiLi'
@@ -25,10 +25,36 @@ print(a, b, c)
 #Mandamos el mismo valor de una variable
 valor1 = valor2 = valor3 = "Chuchito feliz"
 
-print(valor1, valor2, valor3)
+# print(valor1, valor2, valor3)
 
 #contatenar dos varaibles
 inicio = 'Hola '
 final = 'Mundo'
 
 print(inicio + final)
+
+Palabra = 'Esto es una palabra' #tipo String
+Oracion = "Esto tambien es un string solo con comillas dobles" #tipo String
+
+Entero = 1 #Intiger
+ConDecimal = 20.0 #float
+
+Complejo = 1j #numero complejo
+
+# print(Palabra, Oracion, Entero, ConDecimal,Complejo)
+
+
+#creando listas array()
+
+Lista = [1,2,3]
+
+#imprmimos lista
+
+print(Lista)
+
+Lista.append(4) #añadimos nuevos atributos en las lista
+print(Lista)
+
+#copiamos la lista1 con la lista 2
+Lista_2 = Lista.copy()
+print(Lista_2)
