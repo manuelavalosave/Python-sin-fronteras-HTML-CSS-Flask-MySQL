b4a9d1c6633d502632b5051ca10df2334d02108b
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2021-04-26 15:16:17 -0500

    Create ejercicios.py
    obteniendo datos desde la consola

---
+++ intro-python/ejercicios.py
@@ -0,0 +1,8 @@
+dato = input('Ingrese Datos: ');
+#contruimos una lista, para hacer una busqueda
+lista = ['hola','mundo','soy','solitario avalos']
+if lista.count(dato) > 0 :
+    print('El dato existe', dato);
+else:
+    print('El dato no existe :(');
+#print(dato)
