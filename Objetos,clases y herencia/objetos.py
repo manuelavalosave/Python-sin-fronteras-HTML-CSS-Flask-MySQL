470cf4d11732d28ec94034ec793b543e3e350708
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2021-06-29 15:47:41 -0500

    Update objetos.py
    creando metodos

--- Objetos,clases y herencia/objetos.py
+++ Objetos,clases y herencia/objetos.py
@@ -2,7 +2,10 @@ class Usuario:
     def __init__(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
+    
+    #creando un metodo
+    def saludo(self):
+        print('Hola!, mi nombre es', self.nombre, self.apellido)
 
 usuario = Usuario('Solitario','Avalos')
-
-print(usuario.nombre, usuario.apellido)
+usuario.saludo()
